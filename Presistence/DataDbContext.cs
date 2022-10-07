9549302076e5d44d9c619b6bbfaf87745d5f7fb9
nsayed <nsayed@stingrayconsultation.com>
2022-10-07 06:05:13 +0200

    adding data base

---
+++ Presistence/DataDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Presistence;
+public class DataDbContext : DbContext
+{
+    
+
+    public DataDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    public DbSet<Value>Values{ get; set; }
+
+}
